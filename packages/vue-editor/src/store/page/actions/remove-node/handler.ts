60c42ed1a0c942c26e584bd6aaf283ac6386432f
Gerardo Rico Botella <grico@wearemarketing.com>
2020-08-17 19:23:34 +0200

    Close node form on remove

--- packages/vue-editor/src/store/page/actions/remove-node/handler.ts
+++ packages/vue-editor/src/store/page/actions/remove-node/handler.ts
@@ -1,11 +1,21 @@
 import { Action } from 'vuex';
 import RemoveNodePayload from './payload';
 import State from '../../state';
+import { actionTypes as editorActions } from '../../../editor';
+import OpenOnSidebarPayload from '../../../editor/actions/open-on-sidebar/payload';
 
-const handler: Action<State, RemoveNodePayload> = ({}, payload: RemoveNodePayload) => {
+const handler: Action<State, any> = ({dispatch, rootState}, payload: RemoveNodePayload) => {
   const parent = payload.node.parent;
 
   parent.removeChild(payload.node);
+
+  if(payload.node === rootState.editor.sidebarComponentOptions.props.node) {
+    const openOnSidebarPayload: OpenOnSidebarPayload = {
+      component: 'pb-widgets',
+    };
+
+    dispatch('editor/' + editorActions.OPEN_ON_SIDEBAR, openOnSidebarPayload, { root: true });
+  }
 }
 
 export default handler;
